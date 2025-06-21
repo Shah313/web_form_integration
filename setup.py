0e43e8cb92d937ea4d67e400130e5391c0809896
ali <ali@ubuntu-2204.linuxvmimages.local>
2025-06-21 03:44:26 -0400

    twilio integration through steup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
+# setup.py
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in twilio_integration/__init__.py
-from twilio_integration import __version__ as version
+    install_requires = f.read().strip().split('\n')
 
 setup(
-	name='twilio_integration',
-	version=version,
-	description='Custom Frappe Application for Twilio Integration',
-	author='Frappe',
-	author_email='developers@frappe.io',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name='web_form_integration',  # must match folder name
+    version='0.0.1',              # or import from __init__.py
+    description='Twilio SMS Integration for ERPNext',
+    author='Zikpro',
+    author_email='shoaibshah1255@email.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
 )
